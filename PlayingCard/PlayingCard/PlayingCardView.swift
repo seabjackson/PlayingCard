79a27a9ceec4846d02426a76462937533b30deb9
Seab <honeyboyseab@gmail.com>
2018-03-17 12:00:46 -0400

    placed upper left label
    configured upper left label to have an attributed string

--- PlayingCard/PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -25,6 +25,36 @@ class PlayingCardView: UIView {
     private var cornerString: NSAttributedString {
         return centeredAttributedString(rankString+"\n"+suit, fontSize: cornerFontSize)
     }
+    
+    private lazy var upperLeftCornerLabel = createCornerLabel()
+    private lazy var lowerRightCornerLabel = createCornerLabel()
+    
+    private func createCornerLabel() -> UILabel {
+        let label = UILabel()
+        label.numberOfLines = 0
+        addSubview(label)
+        return label
+    }
+    
+    private func configureCornerLabel(_ label: UILabel) {
+        label.attributedText = cornerString
+        label.frame.size = CGSize.zero
+        label.sizeToFit()
+        label.isHidden = !isFaceUp
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureCornerLabel(upperLeftCornerLabel)
+        upperLeftCornerLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
+        
+        configureCornerLabel(lowerRightCornerLabel)
+        lowerRightCornerLabel.frame.origin = CGPoint(x: bounds.maxX, y: bounds.maxY)
+        .offsetBy(dx: -cornerOffset, dy: -cornerOffset)
+        .offsetBy(dx: -lowerRightCornerLabel.frame.size.width, dy: -lowerRightCornerLabel.frame.size.height)
+    }
+    
     override func draw(_ rect: CGRect) {
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         roundedRect.addClip()
@@ -67,8 +97,31 @@ extension PlayingCardView {
     }
 }
 
+extension CGRect {
+    private var leftHalf: CGRect {
+        return CGRect(x: minX, y: minY, width: width / 2, height: height)
+    }
+    
+    private var rightHalf: CGRect {
+        return CGRect(x: midX, y: minY, width: width / 2, height: height)
+    }
+    
+    func inset(by size: CGSize) -> CGRect {
+        return insetBy(dx: size.width, dy: size.height)
+    }
+    
+    func zoom(by scale: CGFloat) -> CGRect {
+        let newWidth = width * scale
+        let newHeight = height * scale
+        return insetBy(dx: (width - newWidth) / 2 , dy: (height - newHeight) / 2)
+    }
+}
 
-
+extension CGPoint {
+    func offsetBy(dx: CGFloat, dy: CGFloat) -> CGPoint {
+        return CGPoint(x: x+dx, y: y+dy)
+    }
+}
 
 
 
