411fc8551bf2734702f04283dd0d87d9d26b0c67
Seab <honeyboyseab@gmail.com>
2018-03-26 21:49:27 -0400

    added a collection outlet
    changed the view controller to have a collection of cards rather than a single playing card

--- PlayingCard/PlayingCard/ViewController.swift
+++ PlayingCard/PlayingCard/ViewController.swift
@@ -11,35 +11,24 @@ import UIKit
 class ViewController: UIViewController {
     
     var deck = PlayingCardDeck()
-
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left,.right]
-            playingCardView.addGestureRecognizer(swipe)
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-            playingCardView.addGestureRecognizer(pinch)
-        }
-    }
     
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFaceUp = !playingCardView.isFaceUp
-        default:
-            break
-        }
-    }
-    @objc func nextCard() {
-        if let card = deck.draw() {
-           playingCardView.rank = card.rank.order
-           playingCardView.suit = card.suit.rawValue
-        }
-    }
+    @IBInspectable
+    @IBOutlet private var cardViews: [PlayingCardView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        var cards = [PlayingCard]()
+        for _ in 1...((cardViews.count + 1) / 2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        
+        for cardView in cardViews {
+            cardView.isFaceUp = true
+            let card = cards.remove(at: cards.count.arc4Random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+        }
     }
 
 
